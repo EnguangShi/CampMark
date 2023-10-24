5d16325ebe6c6bdba60e2836bb990ef4d305b584
EnguangShi <samuelshi20@outlook.com>
2023-10-23 23:47:54 -0700

    Create the route handler for posting a review

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require("./utilities/catchAsync");
 const ExpressError = require("./utilities/ExpressError");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/CampMark")
@@ -38,6 +39,8 @@ const validateCampground = (req, res, next) => {
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+// CAMPGROUND ROUTES
 
 // show all campgrounds
 app.get(
@@ -104,6 +107,20 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+// REVIEW ROUTES
+
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
