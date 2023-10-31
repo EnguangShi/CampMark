1c03961b9daca12eccfc7eb38e45a94561effd52
EnguangShi <samuelshi20@outlook.com>
2023-10-30 17:51:13 -0700

    Associate users with reviews

--- middleware.js
+++ middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require("./schemas");
 const ExpressError = require("./utilities/ExpressError");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 module.exports.isLoggedIn = (req, res, next) => {
   if (!req.isAuthenticated()) {
@@ -15,6 +16,18 @@ module.exports.isAuthor = async (req, res, next) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   if (!campground.author.equals(req.user_id)) {
+    req.flash("error", "You do not have permission to do that!");
+    return res.redirect(`/campgrounds/${id}`);
+  }
+  next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+  const { id, reviewId } = req.params;
+  const review = await Review.findById(reviewId);
+  console.log(review.author);
+  console.log(req.user_id);
+  if (!review.author.equals(req.user._id)) {
     req.flash("error", "You do not have permission to do that!");
     return res.redirect(`/campgrounds/${id}`);
   }
